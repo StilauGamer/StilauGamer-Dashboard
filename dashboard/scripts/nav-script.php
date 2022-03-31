2f1f2ed3539fbd8ea0846d74ed8c5d02b862939f
root <root@_HOSTNAME_>
2022-03-31 09:30:30 +0200

    Added so it always displays error and that the include goes to the correct mysql file

--- dashboard/scripts/nav-script.php
+++ dashboard/scripts/nav-script.php
@@ -1,8 +1,14 @@
 <?php
 
-include "../../mysql.php";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$stmt = $conn->prepare("SELECT title, location FROM nav-list");
+include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
+
+$stmt = $conn->prepare("SELECT title, location FROM navlist");
 $stmt->execute();
+
+return $stmt;
 
 ?>

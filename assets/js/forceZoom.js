c760cae08642cac6d3a619c2f18b722248705f49
root <root@_HOSTNAME_>
2022-04-06 23:44:43 +0200

    Deleted an old forceZoom.js file

--- assets/js/forceZoom.js
+++
@@ -1,4 +0,0 @@
-var scale = 'scale(0.75)';
-document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
- document.body.style.msTransform =   scale;       // IE 9
- document.body.style.transform = scale;     // General

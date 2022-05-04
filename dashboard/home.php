76af2c19161db9252fddc19b034609ae4602d53c
root <root@_HOSTNAME_>
2022-05-04 17:21:23 +0200

    Changed so the pagename variable starts with uppercase and removed the navitems function

--- dashboard/home.php
+++ dashboard/home.php
@@ -1,29 +1,15 @@
 <?php
 
 session_start();
-include "scripts/nav-script.php";
+include_once "scripts/nav-script.php";
 
 $_SESSION["pageName"] = basename($_SERVER["PHP_SELF"], ".php");
-$pageName = $_SESSION["pageName"];
+$pageName = ucfirst($_SESSION["pageName"]);
 
 if(!isset($_SESSION["loggedin"])) {
     header("location: ../login");
     exit();
 }
-
-function navItems($stmt) {
-    $navItems = "";
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $table_title = $row['title'];
-        $table_location = $row['location'];
-        $table_permission = $row['permission'];
-        if ($table_permission == isset($_SESSION[$table_permission])) {
-            $navItems .= "<a href={$table_location}><h1>{$table_title}</h1></a>";
-        } 
-    }
-    return $navItems;
-}
-$navItems = navItems($stmt);
 ?>
 
 <!DOCTYPE html>
@@ -42,26 +28,22 @@ $navItems = navItems($stmt);
     <nav>
         <section id="nav-title">
             <h1 class="title">Nav Title</h1>
-            <hr class="underline">
         </section>
         <section id="nav-content">
             <?php echo $navItems; ?>
         </section>
         <section id="nav-footer">
-            <hr class="underline">
             <h1 class="title">nav footer</h1>
         </section>
     </nav>
     <main>
-        <section id="main-title" onClick="dropdownFunc()">
+        <section id="main-title">
             <h1 class="main-title"><?php echo $pageName?></h1>
             <div class="dropdown">
-                <button class="dropdown-button"><?php echo $pageName ?></button>
+                <button class="dropdown-button" onclick="dropdownFunc()"><?php echo $pageName ?></button>
                 <div class="dropdown-content">
-                    <hr class="underline">
                     <?php echo $navItems; ?>
-                    <hr class="underline">
-                    <h1 href="../logout" class="dropdown-footer">Log Out</a>
+                    <a href="../logout" class="dropdown-footer">Log Out</a>
                 </div>
             </div>
         </section>
@@ -73,7 +55,7 @@ $navItems = navItems($stmt);
 <style>
 <?php
 if ($_SESSION["settings_layout"] == 2) {
-    include("../css/dashboard/layouts/layout2.css");
+    include_once("../css/dashboard/layouts/layout2.css");
 }
 ?>
 </style>

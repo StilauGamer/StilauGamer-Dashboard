d309ca34360868f162ccc701c53416f909736f3b
root <root@_HOSTNAME_>
2022-05-05 15:50:09 +0200

    Redone the whole login script. - Fixed for a more cleaner code. - Made it check for hashed passwords instead of default passwords for better security. - Better system overall

---
+++ login-script-V2.php
@@ -0,0 +1,51 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include "mysql.php";
+
+$username = trim($_POST["username"]);
+$password = trim($_POST["password"]);
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (empty($username) && empty($password)) {
+        header("location: ./login?error=Please enter a username and a password.");
+        exit();
+    }
+    if (empty($username)) {
+        header("location: ./login?error=Please enter a username.");
+        exit();
+    }
+    if (empty($password)) {
+        header("location: ./login?error=Please enter a password.");
+        exit();
+    }
+
+    if ($stmt = $conn->prepare("SELECT * FROM users WHERE username=:uname")) {
+        $stmt->bindParam(":uname", $username);
+        $stmt->execute();
+        $rowCount = $stmt->rowCount();
+        if ($rowCount == 1) {
+            $result = $stmt->fetch();
+            $hashed_password = trim($result["hashed_password"]);
+            if (password_verify($password, $hashed_password)) {
+                session_start();
+                $_SESSION["loggedin"] = true;
+                $_SESSION["user_id"] = $result["user_id"];
+                header("location: ./dashboard/home");
+            }
+        } else {
+            header("location: ./login?error=Invalid username or password.");
+            exit();
+        }
+    } else {
+        header("location: ./login?error=wrong");
+        exit();
+    }
+}
+
+?>

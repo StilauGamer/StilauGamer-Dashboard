5a3c43042ae84cd36b2c56ea0d31d50999d1b35b
root <root@_HOSTNAME_>
2022-05-06 01:18:15 +0200

    Removed the checkPerm function from the currentpage-script file

--- dashboard/scripts/currentpage-script.php
+++ dashboard/scripts/currentpage-script.php
@@ -16,31 +16,4 @@ function setCurrentPage($user_id, $page) {
     $stmt->bindParam(":user_id", $user_id);
     $stmt->execute();
 }
-
-
-
-
-function checkPerm($user_id, $perm_id) {
-    include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
-    $stmt = $conn->prepare("SELECT * FROM users WHERE user_id=:user_id");
-    $stmt->bindParam(":user_id", $user_id);
-    $stmt->execute();
-    $stmt2 = $conn->prepare("SELECT * FROM permissions WHERE perm_id=:perm_id");
-    $stmt2->bindParam(":perm_id", $perm_id);
-    $stmt2->execute();
-    if ($stmt->rowCount() == 1 AND $stmt2->rowCount() == 1) {
-        $stmt3 = $conn->prepare("Select * FROM roles WHERE user_id=:user_id AND perm_id=:perm_id");
-        $stmt3->bindParam(":user_id", $user_id);
-        $stmt3->bindParam(":perm_id", $perm_id);
-        $stmt3->execute();
-        if ($stmt3->rowCount() == 1) {
-            var_dump("You got permission for it.");
-            exit();
-        }
-        var_dump("Both the user and the permission is register, but you're not linked to it.");
-        exit();
-    }
-    var_dump("The user and/or the permission is not registered.");
-    exit();
-}
 ?>

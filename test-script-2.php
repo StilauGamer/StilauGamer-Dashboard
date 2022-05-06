04e3b3efd596742aefe183b29102e92ef41a7388
root <root@_HOSTNAME_>
2022-05-06 18:19:08 +0200

    Created a script to update the theme and layout

---
+++ test-script-2.php
@@ -0,0 +1,22 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+$id = $_POST["id"];
+$theme = $_POST["theme"];
+$layout = $_POST["layout"];
+
+include "mysql.php";
+$stmt = $conn->prepare("UPDATE users SET theme=:theme, layout=:layout WHERE user_id=:user_id");
+$stmt->bindParam(":user_id", $id);
+$stmt->bindParam(":theme", $theme);
+$stmt->bindParam(":layout", $layout);
+$stmt->execute();
+$_SESSION["settings_layout"] = $layout;
+$_SESSION["settings_theme"] = $theme;
+
+?>

ed5c233e581f537596815e58eb8dddb89bf85724
root <root@_HOSTNAME_>
2022-05-13 00:33:49 +0200

    Few changes to the new user script. - Made the file into two functions, loggedIn and getUserInfo. - The getUserInfo gets all the user info and returns it in an array. - The loggedIn function checks if there is a row in the DB that is the same as the hashed_token in the login_token cookie.

--- dashboard/scripts/checkUser-script.php
+++ dashboard/scripts/checkUser-script.php
@@ -1,29 +1,44 @@
 <?php
 
-if (!isset($_COOKIE["login_token"])) {
-    return false;
-    exit();
+function loggedIn() {
+    if (!isset($_COOKIE["login_token"])) {
+        return false;
+        exit();
+    }
+    include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
+    $stmt = $conn->prepare("SELECT user_id FROM sessions WHERE hashed_token=:hashed_token");
+    $stmt->bindParam(":hashed_token", $_COOKIE["login_token"]);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    if ($rowCount == 1) {
+        return true;
+        exit();
+    }
 }
 
-include_once $_SERVER['DOCUMENT_ROOT']."/mysql.php";
-$stmt = $conn->prepare("SELECT user_id FROM sessions WHERE hashed_token=:hashed_token");
-$stmt->bindParam(":hashed_token", $_COOKIE["login_token"]);
-$stmt->execute();
-$result = $stmt->fetch();
 
-if ($result["user_id"] > 0 ) {
-    $stmt2 = $conn->prepare("SELECT * FROM users WHERE user_id=:user_id");
-    $stmt2->bindParam(":user_id", $result["user_id"]);
-    $stmt2->execute();
-    $result2 = $stmt2->fetch();
-    $userInfo = [
-        "user_id" => $result2["user_id"],
-        "username" => $result2["username"],
-        "settings_theme" => $result2["theme"],
-        "settings_layout" => $result2["layout"],
-    ];
-    return $userInfo;
+function getUserInfo() {
+    include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
+    $stmt = $conn->prepare("SELECT user_id FROM sessions WHERE hashed_token=:hashed_token");
+    $stmt->bindParam(":hashed_token", $_COOKIE["login_token"]);
+    $stmt->execute();
+    $result = $stmt->fetch();
+
+    if ($result["user_id"] > 0 ) {
+        $stmt2 = $conn->prepare("SELECT * FROM users WHERE user_id=:user_id");
+        $stmt2->bindParam(":user_id", $result["user_id"]);
+        $stmt2->execute();
+        $result2 = $stmt2->fetch();
+        $userInfo = [
+            "user_id" => $result2["user_id"],
+            "username" => $result2["username"],
+            "settings_theme" => $result2["theme"],
+            "settings_layout" => $result2["layout"],
+        ];
+        return $userInfo;
+    }
 }
+
 
 
 ?>

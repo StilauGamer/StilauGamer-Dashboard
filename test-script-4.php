1fd13299a8a592f1c8f4d496ad3ba9893104f4fa
root <root@_HOSTNAME_>
2022-05-12 09:05:11 +0200

    Old test script for the test page for updating users.

---
+++ test-script-4.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+$id = $_POST["id"];
+$username = $_POST["username"];
+$theme = $_POST["theme"];
+$layout = $_POST["layout"];
+
+include "./mysql.php";
+$stmt = $conn->prepare("UPDATE users SET username=:username, layout=:layout, theme=:theme WHERE user_id=:user_id");
+$stmt->bindParam(":user_id", $id);
+$stmt->bindParam(":username", $username);
+$stmt->bindParam(":theme", $theme);
+$stmt->bindParam(":layout", $layout);
+$stmt->execute();
+$_SESSION["settings_layout"] = $layout;
+$_SESSION["settings_theme"] = $theme;
+header("location: ./test");
+
+?>

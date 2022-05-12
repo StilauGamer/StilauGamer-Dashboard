8cd345547013cf6a66797a225f74cab79070dd91
root <root@_HOSTNAME_>
2022-05-12 09:07:31 +0200

    Big changes to the login script. - Added a check to check if there is a cookie in the browser. - Changed from saving the session variables to saving cookies and generating a random hash code for the user. - Added a saveCookieDb function that saves the user id and the hashed token in a sessions table in the database.

--- login-script-V2.php
+++ login-script-V2.php
@@ -4,55 +4,66 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include "mysql.php";
-
 $username = trim($_POST["username"]);
 $password = trim($_POST["password"]);
 
+# Cookie
+if (isset($_COOKIE["login_token"])) {
+    header("location: ./dashboard/home");
+}
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+# No Cookie
+if (!isset($_COOKIE["login_token"])) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        include_once "mysql.php";
 
-    if (empty($username) && empty($password)) {
-        header("location: ./login?error=Please enter a username and a password.");
-        exit();
-    }
-    if (empty($username)) {
-        header("location: ./login?error=Please enter a username.");
-        exit();
-    }
-    if (empty($password)) {
-        header("location: ./login?error=Please enter a password.");
-        exit();
-    }
+        if (empty($username) && empty($password)) {
+            header("location: ./login?error=Please enter a username and a password.");
+            exit();
+        }
+        if (empty($username)) {
+            header("location: ./login?error=Please enter a username.");
+            exit();
+        }
+        if (empty($password)) {
+            header("location: ./login?error=Please enter a password.");
+            exit();
+        }
 
-    if ($stmt = $conn->prepare("SELECT * FROM users WHERE username=:uname")) {
-        $stmt->bindParam(":uname", $username);
-        $stmt->execute();
-        $rowCount = $stmt->rowCount();
-        if ($rowCount == 1) {
-            $result = $stmt->fetch();
-            $hashed_password = trim($result["hashed_password"]);
-            if (password_verify($password, $hashed_password)) {
-                session_start();
-                $_SESSION["loggedin"] = true;
-                $_SESSION["user_id"] = $result["user_id"];
-                $_SESSION["username"] = $result["username"];
-                $_SESSION["settings_theme"] = $result["theme"];
-                $_SESSION["settings_layout"] = $result["layout"];
-                header("location: ./dashboard/home");
+        if ($stmt = $conn->prepare("SELECT * FROM users WHERE username=:uname")) {
+            $stmt->bindParam(":uname", $username);
+            $stmt->execute();
+            $rowCount = $stmt->rowCount();
+            if ($rowCount == 1) {
+                $result = $stmt->fetch();
+                $hashed_password = trim($result["hashed_password"]);
+                if (password_verify($password, $hashed_password)) {
+                    $hashed_token = md5(sha1($result["user_id"] . $hashed_password));
+                    setcookie("login_token", $hashed_token, time() + 86400);
+                    saveCookieDb($result["user_id"], $hashed_token);
+                    header("location: ./dashboard/home");
+                } else {
+                    header("location: ./login?error=Wrong username or password.");
+                    exit();
+
+                }
             } else {
                 header("location: ./login?error=Wrong username or password.");
                 exit();
-
             }
         } else {
-            header("location: ./login?error=Wrong username or password.");
+            header("location: ./login?error=A problem has occured, try again later.");
             exit();
         }
-    } else {
-        header("location: ./login?error=A problem has occured, try again later.");
-        exit();
     }
+}
+
+function saveCookieDb($user_id, $hashed_token) {
+    include "mysql.php";
+    $stmt = $conn->prepare("INSERT INTO sessions VALUES(:user_id, :hashed_token)");
+    $stmt->bindParam(":user_id", $user_id);
+    $stmt->bindParam(":hashed_token", $hashed_token);
+    $stmt->execute();
 }
 
 ?>

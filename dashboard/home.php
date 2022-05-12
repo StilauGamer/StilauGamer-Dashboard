32a08878f1f9170a61a52d65733e3c4a46e77746
root <root@_HOSTNAME_>
2022-05-12 09:01:04 +0200

    Big updates to the homepage. - Removed the session variables, etc to the new checkuser script. - Added a textColor varialbe. - Added an error section ( Still WIP, might be deleted. ) - Removed the old error section that was in the navbar. - Added design for the themes in the style statement.

--- dashboard/home.php
+++ dashboard/home.php
@@ -1,22 +1,18 @@
 <?php
-
-session_start();
 include_once "scripts/nav-script.php";
 include_once "scripts/theme-script.php";
 include_once "scripts/currentpage-script.php";
 
 # Checks if the user is logged in.
-if(!isset($_SESSION["loggedin"])) {
-    header("location: ../login");
-    exit();
-}
+include_once "scripts/checkUser-script.php";
 
 # Updates the DB and tells what page the user is on.
-setCurrentPage($_SESSION["user_id"], getCurrentPage());
-$backgroundColor = getThemeColor($_SESSION["settings_theme"], "background-color", null);
-$mainColor = getThemeColor($_SESSION["settings_theme"], "main-color", null);
-$boxColor = getThemeColor($_SESSION["settings_theme"], "box-color", null);
-$lineColor = getThemeColor($_SESSION["settings_theme"], "line-color", null);
+setCurrentPage($userInfo["user_id"], getCurrentPage());
+$backgroundColor = getThemeColor($userInfo["settings_theme"], "background-color", null);
+$mainColor = getThemeColor($userInfo["settings_theme"], "main-color", null);
+$boxColor = getThemeColor($userInfo["settings_theme"], "box-color", null);
+$textColor = getThemeColor($userInfo["settings_theme"], "text-color", null);
+$lineColor = getThemeColor($userInfo["settings_theme"], "line-color", null);
 
 ?>
 
@@ -37,47 +33,66 @@ $lineColor = getThemeColor($_SESSION["settings_theme"], "line-color", null);
     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
 </head>
 <body>
-    <nav>
-        <section id="nav-title">
-            <h1 class="title"><?php echo $_SESSION["username"] ?></h1>
-        </section>
-        <section id="nav-content">
-            <?php echo $navItems; ?>
-        </section>
-        <section id="error">
-            <?php if(isset($_GET["error"])) { ?>
-                <p class="error"><?php echo $_GET["error"]; ?></p>
-            <?php } ?>
-        </section>
-        <section id="nav-footer">
-            <a href="../logout">Log Out</a>
-        </section>
-    </nav>
-    <main>
-        <section id="main-title">
-            <h1 class="main-title"><?php echo getCurrentPage(); ?></h1>
-            <div class="dropdown">
-                <button class="dropdown-button" onclick="dropdownFunc()"><?php echo getCurrentPage(); ?></button>
-                <div class="dropdown-content">
-                    <?php echo $navItems; ?>
-                    <a href="../logout" class="dropdown-footer">Log Out</a>
+    <?php if (isset($_GET["error-title"])) { ?>
+    <section id="error">
+        <div class="grid-title">
+            <h1><?php echo $_GET["error-title"] ?></h1>
+        </div>
+        <div class="grid-close">
+            <a href="./<?php echo lcfirst(getCurrentPage()) ?>">x</a>
+        </div>
+        <div class="grid-content">
+            <h1><?php echo $_GET["error-content"] ?></h1>
+        </div>
+    </section>
+    <?php } ?>
+    <section id="dashboard">
+        <nav>
+            <section id="nav-title">
+                <h1 class="title"><a href="youtube.com"><?php echo $userInfo["username"] ?></a></h1>
+            </section>
+            <section id="nav-content">
+                <?php echo $navItems; ?>
+            </section>
+            <section id="nav-footer">
+                <a href="../logout">Log Out</a>
+            </section>
+        </nav>
+        <main>
+            <section id="main-title">
+                <h1 class="main-title"><?php echo getCurrentPage(); ?></h1>
+                <div class="dropdown">
+                    <button class="dropdown-button" onclick="dropdownFunc()"><?php echo getCurrentPage(); ?></button>
+                    <div class="dropdown-content">
+                        <?php echo $navItems; ?>
+                        <a href="../logout" class="dropdown-footer">Log Out</a>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <section id="main-content">
-            <h1>main-content</h1>
-        </section>
-    </main>
+            </section>
+            <section id="main-content">
+                <h1>main-content</h1>
+            </section>
+        </main>
+    </section>
 </body>
 <style>
 <?php
-if ($_SESSION["settings_layout"] == 2) {
+if ($userInfo["settings_layout"] == 2) {
     include_once("../css/dashboard/layouts/layout2.css");
 }
 ?>
 
-body {
+.active {
     background: <?php echo $backgroundColor ?>;
+}
+
+#error {
+    background: <?php echo $backgroundColor ?>;
+    color: <?php echo $textColor ?>;
+}
+#dashboard {
+    background: <?php echo $backgroundColor ?>;
+    color: <?php echo $textColor ?>;
 }
 nav {
     background: <?php echo $mainColor ?>;
@@ -89,12 +104,21 @@ main {
     background: <?php echo $boxColor ?>;
     border-bottom: 1px solid <?php echo $lineColor ?>;
 }
+#nav-title a {
+    color: <?php echo $textColor ?>;
+}
 #nav-footer {
     background: <?php echo $boxColor ?>;
     border-top: 1px solid <?php echo $lineColor ?>;
 }
+#nav-footer a {
+    color: <?php echo $textColor ?>;
+}
 #nav-footer a:hover {
     color: <?php echo $backgroundColor ?>;
+}
+#nav-content a {
+    color: <?php echo $textColor ?>;
 }
 #nav-content a:hover {
     background: <?php echo $backgroundColor ?>;
@@ -107,12 +131,23 @@ main {
 }
 .dropdown-button {
     border-bottom: 1px solid <?php echo $lineColor ?>;
+    color: <?php echo $textColor ?>;
 }
 .dropdown-content {
     background: <?php echo $backgroundColor ?>;
 }
-.dropdown-content a:hover {
+.dropdown-content a {
+    color: <?php echo $textColor ?>;
+}
+.dropdown-content a:hover:not(:last-child) {
     background: <?php echo $backgroundColor ?>;
+}
+.dropdown-footer {
+    background: <?php echo $boxColor ?>;
+    border-top: 1px solid <?php echo $lineColor ?>;
+}
+.dropdown-footer:hover {
+    color: <?php echo $backgroundColor ?>;
 }
 </style>
 </html>

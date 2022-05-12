8a203f31a90e691b108bd9850e81e0fff1094783
root <root@_HOSTNAME_>
2022-05-12 08:57:28 +0200

    Changes to the navbar. - Included the currentpage and checkuser script. - Made all include to include_once. - Added a second variable to the navItems function. the user_id. - Added an if variable where it checks if the nav title is the same as the currentpage. If it is, it returns active. - Added a class statement to the navitems. There it checks for the if variable. - Changed the user_id in the checkPerm to the user_id of the cookie. - Added the second function variable when calling it before returning.

--- dashboard/scripts/nav-script.php
+++ dashboard/scripts/nav-script.php
@@ -3,29 +3,33 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
-include "permission-script.php";
+include_once $_SERVER['DOCUMENT_ROOT']."/mysql.php";
+include_once "permission-script.php";
+include_once "currentpage-script.php";
+include_once "checkUser-script.php";
 
 $stmt = $conn->prepare("SELECT title, location, permission FROM navlist");
 $stmt->execute();
 
-function navItems($stmt) {
+function navItems($stmt, $user_id) {
     $navItems = "";
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $table_title = $row['title'];
         $table_location = $row['location'];
         $table_permission = $row['permission'];
+        $active = ($table_title ==
+        getCurrentPage()) ? "active" : null;
         if ($table_permission == null) {
-            $navItems .= "<a href={$table_location}>{$table_title}</a>";
+            $navItems .= "<a href={$table_location} class={$active}>{$table_title}</a>";
         }
-        if (checkPerm($_SESSION["user_id"], getPermId($table_permission))) {
+        if (checkPerm($user_id, getPermId($table_permission))) {
             $navItems .= "<a href={$table_location}>{$table_title}</a>";
         } 
     }
     return $navItems;
 }
 
-$navItems = navItems($stmt);
+$navItems = navItems($stmt, $userInfo["user_id"]);
 
 return $navItems;
 ?>

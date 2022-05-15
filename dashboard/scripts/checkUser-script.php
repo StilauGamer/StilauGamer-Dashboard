1567b824795d9662e23818770b6288d0b0a3bf2b
root <root@_HOSTNAME_>
2022-05-15 15:21:22 +0200

    Big changes to the checkuser script. - add: Added a currentTime variable on loggedin function. - add: Added a AND to the query that checks if the sessionEnd in the database is greater than the actual time.
    - add: Added a new getUserId function that takes the hashed_token in the cookie and returns the ID of that user from the sessions table.
    - add: Added a new getUserInfo function that takes a user_id variable.
    - add: Made it so that it takes the user_id variable and returns the information about the user in an array and returns the array.

--- dashboard/scripts/checkUser-script.php
+++ dashboard/scripts/checkUser-script.php
@@ -6,8 +6,10 @@ function loggedIn() {
         exit();
     }
     include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
-    $stmt = $conn->prepare("SELECT user_id FROM sessions WHERE hashed_token=:hashed_token");
+    $currentTime = date('Y-m-d H:i:s', time());
+    $stmt = $conn->prepare("SELECT user_id FROM sessions WHERE hashed_token=:hashed_token AND sessionEnd>:sessionEnd");
     $stmt->bindParam(":hashed_token", $_COOKIE["login_token"]);
+    $stmt->bindParam(":sessionEnd", $currentTime);
     $stmt->execute();
     $rowCount = $stmt->rowCount();
     if ($rowCount == 1) {
@@ -16,17 +18,26 @@ function loggedIn() {
     }
 }
 
-
-function getUserInfo() {
+function getUserId() {
+    if (!isset($_COOKIE["login_token"])) {
+        return false;
+        exit();
+    }
     include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
-    $stmt = $conn->prepare("SELECT user_id FROM sessions WHERE hashed_token=:hashed_token");
+    $currentTime = date('Y-m-d H:i:s', time());
+    $stmt = $conn->prepare("SELECT user_id FROM sessions WHERE hashed_token=:hashed_token AND sessionEnd>:sessionEnd");
     $stmt->bindParam(":hashed_token", $_COOKIE["login_token"]);
+    $stmt->bindParam(":sessionEnd", $currentTime);
     $stmt->execute();
     $result = $stmt->fetch();
+    return $result["user_id"];
+}
 
-    if ($result["user_id"] > 0 ) {
+function getUserInfo($user_id) {
+    if ($user_id > 0 ) {
+        include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
         $stmt2 = $conn->prepare("SELECT * FROM users WHERE user_id=:user_id");
-        $stmt2->bindParam(":user_id", $result["user_id"]);
+        $stmt2->bindParam(":user_id", $user_id);
         $stmt2->execute();
         $result2 = $stmt2->fetch();
         $userInfo = [

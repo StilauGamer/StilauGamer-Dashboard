2d079d9a48c250b8d7baf97122d02041e1ff0ae1
root <root@_HOSTNAME_>
2022-05-29 06:11:45 +0200

    fix: made it possible to create accounts with emails

--- test-script.php
+++ test-script.php
@@ -6,13 +6,15 @@ error_reporting(E_ALL);
 
 $id = $_POST["id"];
 $username = $_POST["username"];
+$email = $_POST["email"];
 $pass = $_POST["pass"];
 $hashed_pass = password_hash($pass, PASSWORD_DEFAULT);
 
 include "mysql.php";
 
-$stmt = $conn->prepare("INSERT INTO users(username, hashed_password) VALUES(:username, :pass)");
+$stmt = $conn->prepare("INSERT INTO users(username, email, hashed_password) VALUES(:username, :email, :pass)");
 $stmt->bindParam(":username", $username);
+$stmt->bindParam(":email", $email);
 $stmt->bindParam(":pass", $hashed_pass);
 $stmt->execute();
 

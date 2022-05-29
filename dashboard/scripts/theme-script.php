6477beaacaf4ca59c67d113541426162092533d1
root <root@_HOSTNAME_>
2022-05-29 04:17:47 +0200

    fix: Removed the possibility to get rgb values from the theme script.

--- dashboard/scripts/theme-script.php
+++ dashboard/scripts/theme-script.php
@@ -1,34 +1,7 @@
 <?php
 
-function getThemeColor($themeid, $colorName, $type) {
+function getThemeColor($themeid, $colorName) {
     include $_SERVER['DOCUMENT_ROOT']."/mysql.php";
-    if (!$type == "rgb") {
-        if ($stmt = $conn->prepare("SELECT * FROM themes WHERE theme_id=:theme_id")) {
-            $stmt->bindParam(":theme_id", $themeid);
-            $stmt->execute();
-            $result = $stmt->fetch();
-            switch ($colorName) {
-                case "background-color":
-                    return $result["background-color"];
-                    break;
-                case "main-color":
-                    return $result["main-color"];
-                    break;
-                case "box-color":
-                    return $result["box-color"];
-                    break;
-                case "text-color":
-                    return $result["text-color"];
-                    break;
-                case "line-color":
-                    return $result["line-color"];
-                    break;
-                default:
-                    return "#FFFFFF";
-                    break;
-            }
-        }
-    }
     if ($stmt = $conn->prepare("SELECT * FROM themes WHERE theme_id=:theme_id")) {
         $stmt->bindParam(":theme_id", $themeid);
         $stmt->execute();
@@ -45,6 +18,9 @@ function getThemeColor($themeid, $colorName, $type) {
                 break;
             case "text-color":
                 return $result["text-color"];
+                break;
+            case "line-color":
+                return $result["line-color"];
                 break;
             default:
                 return "#FFFFFF";

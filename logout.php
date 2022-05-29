d4743b4b213dc35a828f2a197fe3b9311c47a8d9
root <root@_HOSTNAME_>
2022-05-29 03:55:40 +0200

    fix: Removed the sql part in the logout

--- logout.php
+++ logout.php
@@ -1,8 +1,4 @@
 <?php
-include "mysql.php";
-$stmt = $conn->prepare("DELETE FROM sessions WHERE hashed_token=:hashed_token");
-$stmt->bindParam(":hashed_token", $_COOKIE["login_token"]);
-$stmt->execute();
 setcookie("login_token", "", time() - 3600);
 header("location: /");
 ?>

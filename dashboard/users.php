8464f11bc56c77c2434bae4d01fef200835b73ce
root <root@_HOSTNAME_>
2022-05-29 04:16:51 +0200

    Copied the home page and called it users. add: added everything. add: added a grid container. add: added 4 grid items with the header items. add: added a while for fetching all the users and applied the user info to those.

--- dashboard/users.php
+++ dashboard/users.php
@@ -1,16 +1,29 @@
 <?php
+include_once "scripts/nav-script.php";
+include_once "scripts/theme-script.php";
+include_once "scripts/currentpage-script.php";
 
-session_start();
-include "scripts/nav-script.php";
-include "scripts/currentpage-script.php";
-
-if(!isset($_SESSION["loggedin"])) {
+# Checks if the user is logged in.
+if (!loggedIn()) {
     header("location: ../login");
     exit();
 }
+$userInfo = getUserInfo(getUserId());
 
 # Updates the DB and tells what page the user is on.
-setCurrentPage($_SESSION["user_id"], getCurrentPage());
+# setCurrentPage($userInfo["user_id"], getCurrentPage());
+
+# Getting the users
+$stmt = $conn->prepare("SELECT * FROM users");
+$stmt->execute();
+
+# Getting the theme colors
+$backgroundColor = getThemeColor($userInfo["settings_theme"], "background-color");
+$mainColor = getThemeColor($userInfo["settings_theme"], "main-color");
+$boxColor = getThemeColor($userInfo["settings_theme"], "box-color");
+$textColor = getThemeColor($userInfo["settings_theme"], "text-color");
+$lineColor = getThemeColor($userInfo["settings_theme"], "line-color");
+
 ?>
 
 <!DOCTYPE html>
@@ -19,60 +32,145 @@ setCurrentPage($_SESSION["user_id"], getCurrentPage());
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StilauGamer | Home</title>
+    <title>StilauGamer | <?php echo getCurrentPage(); ?></title>
     <link rel="stylesheet" type="text/css" href="../css/dashboard/dashboard.css">
     <link rel="stylesheet" type="text/css" href="../css/dashboard/dashboard-nav.css">
     <link rel="stylesheet" type="text/css" href="../css/dashboard/dashboard-phone.css">
+    <link rel="stylesheet" type="text/css" href="../css/dashboard/users-table.css">
+    <script src="../assets/js/dropdown.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+
+    <!-- Discord -->
+    <meta content="StilauGamer | <?php echo getCurrentPage() ?>" property="og:title" />
+    <meta content="StilauGamers Dashboard" property="og:description" />
+    <meta content="xampp.stilaugamer.com/dashboard/home" property="og:url" />
+    <meta content="#FFFFFF" data-react-helmet="true" name="theme-color" />
 </head>
 <body>
     <nav>
         <section id="nav-title">
-            <h1 class="title">Nav Title</h1>
-            <hr class="underline">
+            <h1 class="title"><a href="youtube.com"><?php echo $userInfo["username"] ?></a></h1>
         </section>
         <section id="nav-content">
-            <?php
-            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                $table_title = $row['title'];
-                $table_location = $row['location'];
-                $table_permission = $row['permission'];
-                if ($table_permission == isset($_SESSION[$table_permission])) {
-            ?>
-            <a href="<?php echo $table_location; ?>"><h1><?php echo $table_title; ?></h1></a>
-            <?php } } ?>
+            <?php echo $navItems; ?>
         </section>
         <section id="nav-footer">
-            <hr class="underline">
-            <h1 class="title">nav footer</h1>
+            <a href="../logout">Log Out</a>
         </section>
     </nav>
     <main>
         <section id="main-title">
-            <h1 class="main-title"><?php echo $pageName?></h1>
+            <h1 class="main-title"><?php echo getCurrentPage(); ?></h1>
             <div class="dropdown">
-                <button class="dropdown-button"><?php echo $pageName ?></button>
+                <button class="dropdown-button" onclick="dropdownFunc()"><?php echo getCurrentPage(); ?></button>
                 <div class="dropdown-content">
-                    <hr class="underline">
-                    <a>Test</a>
-                    <?php
-                    while($row2 = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                        var_dump($row2);
-                        exit();
-                    }
-                    ?>
+                    <?php echo $navItems; ?>
+                    <a href="../logout" class="dropdown-footer">Log Out</a>
                 </div>
             </div>
         </section>
         <section id="main-content">
-            <h1>main-content</h1>
+            <section id="grid-container">
+                <!-- Header -->
+                <div class="grid-item">ID</div>
+                <div class="grid-item">Username</div>
+                <div class="grid-item">Email</div>
+                <div class="grid-item">Options</div>
+                <!-- Users -->
+                <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                    <div class="grid-item"><?php echo $row["user_id"] ?></div>
+                    <div class="grid-item"><?php echo $row["username"] ?></div>
+                    <div class="grid-item"><?php echo $row["email"] ?></div>
+                    <div class="grid-item"><button>Edit</button></div>
+                <?php } ?>
+            </section>
         </section>
     </main>
 </body>
 <style>
 <?php
-if ($_SESSION["settings_layout"] == 2) {
-    include("../css/dashboard/layouts/layout2.css");
+if ($userInfo["settings_layout"] == 2) {
+    include_once("../css/dashboard/layouts/layout2.css");
 }
 ?>
+.active {
+    background: <?php echo $backgroundColor ?>;
+}
+body {
+    background: <?php echo $backgroundColor ?>;
+    color: <?php echo $textColor ?>;
+}
+nav {
+    background: <?php echo $mainColor ?>;
+}
+main {
+    background: <?php echo $mainColor ?>;
+}
+#nav-title {
+    background: <?php echo $boxColor ?>;
+    border-bottom: 1px solid <?php echo $lineColor ?>;
+}
+#nav-title a {
+    color: <?php echo $textColor ?>;
+}
+#nav-title a:hover {
+    color: <?php echo $backgroundColor ?>;
+}
+#nav-footer {
+    background: <?php echo $boxColor ?>;
+    border-top: 1px solid <?php echo $lineColor ?>;
+}
+#nav-footer a {
+    color: <?php echo $textColor ?>;
+}
+#nav-footer a:hover {
+    color: <?php echo $backgroundColor ?>;
+}
+#nav-content a {
+    color: <?php echo $textColor ?>;
+}
+#nav-content a:hover {
+    background: <?php echo $backgroundColor ?>;
+}
+#main-title {
+    background: <?php echo $boxColor ?>;
+}
+#main-content {
+    background: <?php echo $backgroundColor ?>;
+    border: 5px solid <?php echo $boxColor ?>;
+}
+.dropdown-button {
+    border-bottom: 1px solid <?php echo $lineColor ?>;
+    color: <?php echo $textColor ?>;
+}
+.dropdown-content {
+    background: <?php echo $backgroundColor ?>;
+}
+.dropdown-content a {
+    color: <?php echo $textColor ?>;
+}
+.dropdown-content a:hover:not(:last-child) {
+    background: <?php echo $backgroundColor ?>;
+}
+.dropdown-footer {
+    background: <?php echo $boxColor ?>;
+    border-top: 1px solid <?php echo $lineColor ?>;
+}
+.dropdown-footer:hover {
+    color: <?php echo $backgroundColor ?>;
+}
+.grid-item:nth-child(-n+4) {
+    background: <?php echo $boxColor ?>;
+    border-bottom: 1px solid <?php echo $lineColor ?>;
+}
+.grid-item>button {
+    color: <?php echo $textColor ?>;
+}
+.grid-item>button:hover {
+    background: <?php echo $mainColor ?>;
+}
 </style>
 </html>
